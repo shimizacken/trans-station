f2f1845c2579df585f47cb5e9963e5495d9e8b84
Shimi Zacken <shimi.zacken@sesam.io>
2025-04-15 20:24:38 +0200

    more stations

--- src/constants/radioStations.ts
+++ src/constants/radioStations.ts
@@ -2,11 +2,11 @@ import { Stations } from '../types/station.types';
 
 export const stations: Stations = [
   {
-    id: '1',
-    name: 'Reshet Bet',
+    id: 'kan-bet',
+    name: 'Kan Bet',
     streamUrl: 'https://kanliveicy.media.kan.org.il/icy/kanbet_mp3',
     logoUrl: 'src/images/logos/reshet-bet.png',
-    description: 'Reshet Bet is a popular Israeli radio station.',
+    description: 'Kan Bet is a popular Israeli radio station.',
     genre: 'News',
     country: 'Israel',
     userPreferences: {
@@ -14,11 +14,23 @@ export const stations: Stations = [
     },
   },
   {
-    id: '2',
+    id: 'galgalatz',
     name: 'Galgalatz',
     streamUrl: 'https://glzwizzlv.bynetcdn.com/glglz_mp3?',
     logoUrl: 'src/images/logos/galgalatz.png',
     description: 'Galgalatz is an Israeli radio station that plays music and news.',
+    genre: 'Music',
+    country: 'Israel',
+    userPreferences: {
+      favorite: false,
+    },
+  },
+  {
+    id: 'galatz',
+    name: 'Galatz',
+    streamUrl: 'https://glzwizzlv.bynetcdn.com/glz_mp3?',
+    logoUrl: 'src/images/logos/galatz.png',
+    description: 'Galatz is an Israeli radio station that plays news.',
     genre: 'Music',
     country: 'Israel',
     userPreferences: {
