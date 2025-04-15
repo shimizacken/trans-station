9e560a9903091347368d4b3140ec89b751ae2910
Shimi Zacken <shimi.zacken@sesam.io>
2025-04-15 13:01:34 +0200

    fix default volume

--- src/containers/VolumeSlider.container.tsx
+++ src/containers/VolumeSlider.container.tsx
@@ -1,4 +1,4 @@
-import React, { useState, forwardRef } from 'react';
+import React, { useState, forwardRef, useEffect } from 'react';
 import { Silver } from 'react-dial-knob';
 
 type RadioWithVolumeProps = {
@@ -7,16 +7,22 @@ type RadioWithVolumeProps = {
 
 export const VolumeSliderContainer = forwardRef<HTMLAudioElement, RadioWithVolumeProps>(
   ({ className }, audioRef) => {
-    const [volume, setVolume] = useState(0);
+    const [volume, setVolume] = useState(0.8);
 
     const handleVolumeChange = (value: number) => {
       const newVolume = value / 100;
       setVolume(newVolume);
 
       if (audioRef && 'current' in audioRef && audioRef.current) {
+        console.log('🚀 ~ handleVolumeChange ~ newVolume:', newVolume);
         audioRef.current.volume = newVolume;
       }
     };
+    useEffect(() => {
+      if (audioRef && 'current' in audioRef && audioRef.current) {
+        audioRef.current.volume = volume;
+      }
+    }, []);
 
     return (
       <Silver
