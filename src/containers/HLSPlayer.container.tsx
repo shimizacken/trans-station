7dfb481425bb78cf7593f203122358b3c30c0429
Shimi Zacken <shimi.zacken@sesam.io>
2025-04-16 21:04:18 +0200

    volume knob

--- src/containers/HLSPlayer.container.tsx
+++ src/containers/HLSPlayer.container.tsx
@@ -1,30 +1,30 @@
 import React, { useEffect, useRef } from 'react';
 import Hls from 'hls.js';
+import { Silver } from 'react-dial-knob';
 
 const hlsUrl = 'https://kan88.media.kan.org.il/hls/live/2024812/2024812/playlist.m3u8';
 
 export const HLSPlayerContainer: React.FC = () => {
   const videoRef = useRef<HTMLVideoElement | null>(null);
   const playButtonRef = useRef<HTMLButtonElement | null>(null);
-  const volumeSliderRef = useRef<HTMLInputElement | null>(null);
-  const [volume, setVolume] = React.useState(0.5);
+  const [volume, setVolume] = React.useState(0.8);
 
   useEffect(() => {
-    if (videoRef.current && playButtonRef.current && volumeSliderRef.current) {
+    if (videoRef.current && playButtonRef.current) {
       if (Hls.isSupported()) {
         const hls = new Hls();
         hls.loadSource(hlsUrl);
         hls.attachMedia(videoRef.current);
-        hls.on(Hls.Events.MANIFEST_PARSED, function () {
-          if (videoRef.current && volumeSliderRef.current) {
-            videoRef.current.volume = parseFloat(volumeSliderRef.current.value);
+        hls.on(Hls.Events.MANIFEST_PARSED, () => {
+          if (videoRef.current) {
+            videoRef.current.volume = volume;
           }
         });
       } else if (videoRef.current.canPlayType('application/vnd.apple.mpegurl')) {
         videoRef.current.src = hlsUrl;
       }
     }
-  }, [videoRef.current, playButtonRef.current, volumeSliderRef.current]);
+  }, [videoRef.current, playButtonRef.current]);
 
   const handlePlayPause = () => {
     if (videoRef.current && playButtonRef.current) {
@@ -38,11 +38,11 @@ export const HLSPlayerContainer: React.FC = () => {
     }
   };
 
-  const handleVolumeChange = () => {
-    if (videoRef.current && volumeSliderRef.current) {
-      const value = parseFloat(volumeSliderRef.current.value);
-      videoRef.current.volume = value;
-      setVolume(value);
+  const handleVolumeChange = (value: number) => {
+    if (videoRef.current) {
+      const newVolume = value / 100;
+      videoRef.current.volume = newVolume;
+      setVolume(newVolume);
     }
   };
 
@@ -53,15 +53,13 @@ export const HLSPlayerContainer: React.FC = () => {
         <button id="playPause" ref={playButtonRef} onClick={handlePlayPause}>
           Play
         </button>
-        <input
-          ref={volumeSliderRef}
-          type="range"
-          id="volume"
-          min="0"
-          max="1"
-          step="0.01"
-          value={volume}
-          onChange={handleVolumeChange}
+        <Silver
+          diameter={180}
+          min={0}
+          max={100}
+          step={10}
+          value={volume * 100}
+          onValueChange={handleVolumeChange}
         />
       </div>
     </div>
