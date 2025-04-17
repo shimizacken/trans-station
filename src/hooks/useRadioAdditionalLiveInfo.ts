6f46637bde1ca4faeba8c5e1acfc4b2681743cae
Shimi Zacken <shimi.zacken@sesam.io>
2025-04-17 20:24:53 +0200

    useRadioAdditionalLiveInfo added

---
+++ src/hooks/useRadioAdditionalLiveInfo.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import { RadioStation } from '../types/station.types';
+
+export const useRadioAdditionalLiveInfo = (radioStation: RadioStation) => {
+  const [radioLiveInfo, setRadioLiveInfo] = useState(null);
+  useEffect(() => {
+    const liveTrackDataUrl = radioStation.streamUrls[0].liveTrackDataUrl;
+
+    if (liveTrackDataUrl) {
+      fetch(liveTrackDataUrl)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then((data) => {
+          setRadioLiveInfo(data);
+        })
+        .catch((error) => {
+          console.error('Error fetching live track data:', error);
+        });
+    }
+  }, []);
+
+  return radioLiveInfo;
+};
