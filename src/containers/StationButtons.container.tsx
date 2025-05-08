520f4d83836f124607672dbcca0574730153d964
Shimi Zacken <shimi.zacken@sesam.io>
2025-05-08 09:38:01 +0200

    fixed: bg color on playing/paused

--- src/containers/StationButtons.container.tsx
+++ src/containers/StationButtons.container.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { RadioStationId, RadioStations } from '../types/station.types';
 import { StationButtons } from '../components/StationButtons.view';
 
 import { useLoadPersistSelectedRadioStation } from '../hooks/useLoadPersistSelectedRadioStation';
+import { mediaElementStatus } from '../signals/mediaElementStatus.signal';
 
 export const StationButtonsContainer: React.FC<{
   stations: RadioStations;
   onClick: (stationId: RadioStationId) => void;
 }> = ({ stations, onClick }) => {
   const station = useLoadPersistSelectedRadioStation(stations);
-  const [selectedStation, setSelectedStation] = React.useState<string>(station.id);
+  const [selectedStation, setSelectedStation] = useState<string>(station.id);
+  const [isStationPlaying, setIsStationPlaying] = useState(false);
+
+  useEffect(() => {
+    mediaElementStatus.watch((status) => {
+      setIsStationPlaying(status === 'playing' ? true : false);
+    });
+  }, []);
 
   const handleStationClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     const stationId = event.currentTarget.id as RadioStationId;
@@ -22,6 +30,7 @@ export const StationButtonsContainer: React.FC<{
       stations={stations}
       selectedStation={selectedStation}
       handleStationClick={handleStationClick}
+      isStationPlaying={isStationPlaying}
     />
   );
 };
