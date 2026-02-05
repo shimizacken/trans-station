5e2e4a3d14f6fd268c3bb50f59cabc31eb021369
Shimi Zacken <shimi.zacken@sesam.io>
2026-02-05 20:56:41 +0100

    dynamic year

--- src/components/Footer.view.tsx
+++ src/components/Footer.view.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Footer: React.FC = () => (
   <footer className="footer">
     <div className="footer-content">
-      <p>Simple streaming radio app 📻. All rights reserved © 2025</p>
+      <p>Simple streaming radio app 📻. All rights reserved © {new Date().getFullYear()}</p>
     </div>
   </footer>
 );
